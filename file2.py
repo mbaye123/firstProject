2f805e3201a9f53ff5de2342f0455d431eb482da
mbaye123 <mbaynd@gmail.com>
2014-11-19 13:57:26 +0100

    Update file2.py
    Test Pull a faire apres un rajout depuis github

--- file2.py
+++ file2.py
@@ -3,4 +3,5 @@
 print "Assalamou Alaika wa RahmatoullALLAH"
 name = input("Enter your first name : ")
 print name + " est votre nom"
+print "Cette ligne est rajoutée depuis l'interface web de GITHUB"
 
